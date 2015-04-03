386337cc07b2e785c1b6740398458b9819ad0da8
onda anan <ondaanan@onda-no-MacBook-Pro.local>
2015-04-03 15:12:04 +0900

    Initial Commit

---
+++ アラート/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  アラート
+//
+//  Created by onda anan on 2015/04/03.
+//  Copyright (c) 2015年 onda anan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ アラート/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  アラート
+//
+//  Created by onda anan on 2015/04/03.
+//  Copyright (c) 2015年 onda anan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
