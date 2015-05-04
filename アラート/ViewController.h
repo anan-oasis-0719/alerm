350154c92663d67a88be3533257f5861baf4a709
onda anan <anan.oasis.0719@gmail.com>
2015-05-04 14:31:53 +0900

    アラームできたああああああ！

--- アラート/ViewController.h
+++ アラート/ViewController.h
@@ -8,8 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITextFieldDelegate>
 
+{
+    int count1;//時間
+    int count2;//分
+    int count3;//秒
+    
+    IBOutlet UILabel *zikanLabel;
+    IBOutlet UILabel *hunLabel;
+    IBOutlet UILabel *byouLabel;
+    
+    IBOutlet UITextField *zikanTextField;
+    IBOutlet UITextField *hunTextField;
+    
+    NSTimer *timer;
+}
+
+-(IBAction)kasutamu;
+-(IBAction)kaijo;
+
+-(void)up;
 
 @end
 
--- アラート/ViewController.m
+++ アラート/ViewController.m
@@ -16,8 +16,106 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    count1=0;//時間
+    count2=0;//分
+    count3=0;//秒
+    
+    zikanTextField.delegate = self ;
+    hunTextField.delegate = self ;
 }
+
+-(IBAction)kasutamu{
+    count1=[zikanTextField.text intValue];
+    count2=[hunTextField.text intValue];
+    count3=0;
+    
+    zikanLabel.text=zikanTextField.text;
+    hunLabel.text=hunTextField.text;
+    byouLabel.text=@"0";
+
+    UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"お知らせ"
+                                                 message:@"タイマーの設定が完了しました。"
+                                                delegate:nil
+                                       cancelButtonTitle:nil
+                                       otherButtonTitles:@"OK", nil
+                        ];
+    [alert show];
+    
+    timer=[NSTimer scheduledTimerWithTimeInterval:1
+                                           target:self
+                                         selector:@selector(up)
+                                         userInfo:nil
+                                          repeats:YES];
+    
+}
+
+-(void)up{
+    count3=count3-1;
+    if (count3==0&&count2==0&&count1==0) {
+        UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"安らぎは終わった"
+                                                     message:@"ささっと働け"
+                                                    delegate:nil
+                                           cancelButtonTitle:nil
+                                           otherButtonTitles:@"OK", nil
+                            ];
+        [alert show];
+        
+        [timer invalidate];}
+    
+    
+    if (count3<=0&&count2!=0) {
+        count3=59;
+        count2=count2-1;
+        }
+    
+    if (count2<=0&&count2!=0) {
+        count2=59;
+        count1=count1-1;
+    }
+    
+    if (count1<=0) {
+        count1=0;
+    }
+        zikanLabel.text=[NSString stringWithFormat:@"%d",count1];
+        hunLabel.text=[NSString stringWithFormat:@"%d",count2];
+        byouLabel.text=[NSString stringWithFormat:@"%d",count3];
+    
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    
+    [textField resignFirstResponder];
+    return YES;
+    
+
+}
+
+-(IBAction)kaijo;
+{
+    if ([timer isValid]) {
+        [timer invalidate];
+        
+        count1=0;//時間
+        count2=0;//分
+        count3=0;//秒
+        
+        zikanLabel.text=[NSString stringWithFormat:@"%d",count1];
+        hunLabel.text=[NSString stringWithFormat:@"%d",count2];
+        byouLabel.text=[NSString stringWithFormat:@"%d",count3];
+        
+        UIAlertView *alert=[[UIAlertView alloc]initWithTitle:@"お知らせ"
+                                                     message:@"解除しました"
+                                                    delegate:nil
+                                           cancelButtonTitle:nil
+                                           otherButtonTitles:@"OK", nil
+                                ];
+            [alert show];
+            
+            [timer invalidate];}
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
